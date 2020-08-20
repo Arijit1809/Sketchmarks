f64d834a47a672c466883fc8f4e222e181546cd5
marksketch <markssketch@gmail.com>
2020-08-21 03:07:26 +0530

    signup form sahi

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,17 +1,27 @@
+let regex= /^[a-z0-9]+$/i
 $(document).ready(function(){
     $("#username").keyup(function(){
         let username=$(this).val()
         $(".user-error").css("visibility","visible")
         if(username.length>4){
-            wait=true
             $.post("/checkusername",{user: $(this).val()},function(data,status){
-                if(data)
+                if(data){
                     $(".user-error").html(`<a style="color: red;">Username unavailable</a>`)
-                else
+                    $("#sub-btn").attr("type","button")
+                }
+                else if(!username.match(regex)){
+                    $(".user-error").html(`<a style="color: red;">Username can only have letters and numbers</a>`)
+                    $("#sub-btn").attr("type","button")
+                }
+                else{
                     $(".user-error").html(`<a style="color: rgb(0, 190, 0);">Username available</a>`)
+                    $("#sub-btn").attr("type","submit")
+                }
             })            
         }
-        else
+        else{
             $(".user-error").html(`<a style="color: red;">Username should be 5 characters at least</a>`)
+            $("#sub-btn").attr("type","button")
+        }
     })
 })
