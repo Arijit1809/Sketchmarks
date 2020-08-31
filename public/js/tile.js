9fcb355e10ed3ee297b7f186a037a481d40eda03
marksketch <markssketch@gmail.com>
2020-09-01 01:04:43 +0530

    a

--- public/js/tile.js
+++ public/js/tile.js
@@ -2,7 +2,6 @@ $(document).ready(()=>{
     let media=matchMedia("(max-width: 980px)")
     if(media.matches){
         $(".submit").css("bottom","70px")
-        // $(".nav-btm").css("bottom", "0")
     }
     $(window).scroll(() => {
         if (this.scrollY > 2){
